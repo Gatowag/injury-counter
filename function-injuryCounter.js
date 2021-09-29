32059e69ac36871942e2054cc5493aee72525c42
Brandt Hughes <brandt@brandthughes.com>
2021-09-28 23:11:10 -0500

    generalized code and wrote helpful comments
    "tab" now refers to whatever tab you've entered it in so that it's easier to implement elsewhere, and I've written various comments to help describe what everything is doing

--- function-injuryCounter.js
+++ function-injuryCounter.js
@@ -6,20 +6,24 @@
 
 function injuryCounter() {
 	const ss = SpreadsheetApp.getActiveSpreadsheet();
-	const tab2 = ss.getSheetByName("Release Order");
+	const tab = ss.getActiveSheet();
 	const millisecondsPerDay = 1000 * 60 * 60 * 24;
 	let thisRange = SpreadsheetApp.getActiveRange();
 	let thisRow = thisRange.getRow();
 	let thisDate = new Date(tab2.getRange("D" + thisRow).getValue());
 	let lastInjury;
 
+	/*	the function is willing to scan through 200 rows
+		until it finds another cell with any values, but
+		will break when it finds one in the for loop ↓
+	*/
 	for (i = 1; i < 200; i++){
-		if (tab2.getRange("J" + (thisRow + i)).getValue() != "") {
-			lastInjury = new Date (tab2.getRange("D" + (thisRow + i)).getValue());
-			break;
+		if (tab.getRange("J" + (thisRow + i)).getValue() != "") {					// what happens when it finds a cell with data:
+			lastInjury = new Date (tab.getRange("D" + (thisRow + i)).getValue());	// it sets lastInjury to the date value found in that row
+			break;																	// then stops the loop
 		};
 	};
 
-	// return thisDate;
+	// outputs number of days since last injury;
 	return Math.floor((Date.parse(thisDate) - Date.parse(lastInjury)) / millisecondsPerDay);
 }
