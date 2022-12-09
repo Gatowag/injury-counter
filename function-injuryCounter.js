0d7eaceef5c9df94e79e57a666aa2a0e131f7084
Brandt Hughes <brandt@brandthughes.com>
2022-12-09 02:13:57 -0600

    optimizations
    Changed column to reflect changes in the prod doc. Optimized by reducing rows searched and requesting cell values one time outside of the for loop.

--- function-injuryCounter.js
+++ function-injuryCounter.js
@@ -10,18 +10,20 @@ function injuryCounter() {
 	const millisecondsPerDay = 1000 * 60 * 60 * 24;
 	let thisRange = SpreadsheetApp.getActiveRange();
 	let thisRow = thisRange.getRow();
-	let thisDate = new Date(tab2.getRange("D" + thisRow).getValue());
+	let thisDate = new Date(tab2.getRange("C" + thisRow).getValue());
 	let lastInjury;
 
-	/*	the function is willing to scan through 200 rows
+	/*	the function is willing to scan through 100 rows
 		until it finds another cell with any values, but
 		will break when it finds one in the for loop ↓
 	*/
-	for (i = 1; i < 200; i++){
+	let allInj = tab.getRange("P" + thisRow + ":P" + (thisRow + 100)).getValues();
+
+	for (i = 1; i < 100; i++){
 		// what happens when it finds a cell with data:
-		if (tab.getRange("P" + (thisRow + i)).getValue() != "") {
+		if (allInj[i] != "") {
 			// sets lastInjury to the date value found in that row
-			lastInjury = new Date(tab.getRange("D" + (thisRow + i)).getValue());
+			lastInjury = new Date(tab.getRange("C" + (thisRow + i)).getValue());
 			// modifies that date to midnight, this way we don't get a miscount
 			lastInjury.setHours(0,0,0,0);
 			// stops the loop
